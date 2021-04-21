39c752d689fa70d0d10b8c9e543655cbbb81199a
Florane <qwerty.qa912@gmail.com>
2021-04-21 14:13:37 +0300

    Linux(gcc)/Windows(VS) compatability

--- tilemap.cpp
+++ tilemap.cpp
@@ -5,12 +5,21 @@ void loadTilemap(const char* name, Tilemap* tilemap)
 {
     FILE* file;
 
-    file = fopen(name, "r");
+    #ifdef __linux__
+        file = fopen(name, "r");
+    #elif _WIN32
+        fopen_s(file, name, "r");
+    #endif
+
     for(int y = 0;y < SIZE_X;y++)
     {
         for(int x = 0;x < SIZE_Y;x++)
         {
-            fscanf(file, "%d", &tilemap->tiles[x][y]);
+            #ifdef __linux__
+                fscanf(file, "%d", &tilemap->tiles[x][y]);
+            #elif _WIN32
+                fscanf_s(file, "%d", &tilemap->tiles[x][y]);
+            #endif
         }
     }
     fclose(file);
@@ -20,7 +29,11 @@ void saveTilemap(const char* name, Tilemap* tilemap)
 {
     FILE* file;
 
-    file = fopen(name, "w+");
+    #ifdef __linux__
+        file = fopen(name, "r");
+    #elif _WIN32
+        fopen_s(file, name, "r");
+    #endif
     for(int y = 0;y < SIZE_X;y++)
     {
         for(int x = 0;x < SIZE_Y;x++)
